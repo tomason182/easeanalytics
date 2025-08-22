963e518084ebb9f3478b7a0cf423a6d461633ba4
Tomas <tomas@debian.com>
2025-08-22 15:51:28 -0300

    add: header, main graph and statics cards to app

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import Graph from "./components/Graph.tsx";
+import TopPages from "./components/Cards/TopPages.tsx";
+import Referres from "./components/Cards/Referres.tsx";
+import Device from "./components/Cards/Device.tsx";
+import Countries from "./components/Cards/Countries.tsx";
+import styles from "./App.module.css"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
+  const mockedData = {
+    "labels": ["2025-08-15", "2025-08-16", "2015-08-17"],
+    "pageViews": [120, 95, 140]
+  }
+
+  const labels = mockedData.labels;
+  const counts = mockedData.pageViews;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className={styles.mainContainer}>
+      <header>
+        <p>Ease Analitics</p>
+        <button className={styles.profileBtn}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide-user-round"><circle cx="12" cy="8" r="5"/><path d="M20 21a8 8 0 0 0-16 0"/></svg>
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      </header>
+      <div className={styles.statsContainer}>
+          <ul className={styles.stats}>
+            <li>
+              <h3>TOTAL VISITS</h3>
+              <span>1.3M</span>
+            </li>
+            <li>
+              <h3>UNIQUE VISITORS</h3>
+              <span>834K</span>
+            </li>
+            <li>
+              <h3>TOTAL PAGES VIEWS</h3>
+              <span>983</span>
+            </li>
+          </ul>
+        <div className="graphContainer">
+         <Graph labels={labels} data={counts} />
+        </div>
+        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      <div className={styles.gridContainer}>
+        <TopPages />
+        <Referres />
+        <Device />
+        <Countries />
+      </div>
+    </div>
+
+ )
 }
 
 export default App
