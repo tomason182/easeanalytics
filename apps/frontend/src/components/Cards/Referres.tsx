248a4a7377f93678a3c0e1dce520e37969d46fc0
Tomas <tomas@debian.com>
2025-08-22 11:01:16 -0300

    add: title to Referres card

--- apps/frontend/src/components/Cards/Referres.tsx
+++ apps/frontend/src/components/Cards/Referres.tsx
@@ -4,22 +4,27 @@ import styles from "./Cards.module.css";
 export default function Referrers() {
   return (
     <div className={styles.card}>
+      <h3>Referres</h3>
       <table>
-        <caption>Total visitors per country last year</caption>
+        <caption>Total visitors per type of source</caption>
         <thead>
           <tr>
-            <th scope="col">Country</th>
+            <th scope="col">Source</th>
             <th scope="col">Visitors</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <th scope="row">Argentina</th>
+            <th scope="row">Direct</th>
             <td>36k</td>
           </tr>
           <tr>
-            <th scope="row">Chile</th>
+            <th scope="row">DuckDuckGo</th>
             <td>25k</td>
+          </tr>
+          <tr>
+            <th scope="row">Google</th>
+            <td>13k</td>
           </tr>
         </tbody>
       </table>
