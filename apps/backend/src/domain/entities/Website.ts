00d9eb25d4131d5a4327958060fd52bde2310d2d
Tomas <tomas@debian.com>
2025-08-25 20:27:13 -0300

    fix: website entity. Remove unnecessary import

--- apps/backend/src/domain/entities/Website.ts
+++ apps/backend/src/domain/entities/Website.ts
@@ -1,6 +1,5 @@
 import { randomUUID } from "node:crypto";
 import { WebsiteDTO } from "../../dto/WebsiteDTO";
-import { promises } from "node:dns";
 
 export class Website {
   public id: number | null;
