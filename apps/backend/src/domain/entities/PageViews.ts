d352475b2a6c2850c056c0dfb74777814d5ba5a1
Tomas <tomas@debian.com>
2025-08-25 20:26:08 -0300

    add: PageViews entity

---
+++ apps/backend/src/domain/entities/PageViews.ts
@@ -0,0 +1,85 @@
+import { DeviceType, PageViewsDTO } from "../../dto/PageViewsDTO";
+
+export class PageViews {
+  public id: number;
+  public websiteId: number;
+  public visitorId: string;
+  public pageUrl: string;
+  public referrer: string;
+  public deviceType: DeviceType;
+  public browser: string;
+  public os: string;
+  public country: string;
+  public viewedAt: Date;
+
+  constructor(
+    id: number,
+    websiteId: number,
+    visitorId: string,
+    pageUrl: string,
+    referrer: string,
+    deviceType: DeviceType,
+    browser: string,
+    os: string,
+    country: string,
+    viewedAt: Date
+  ) {
+    this.id = id;
+    this.websiteId = websiteId;
+    this.visitorId = visitorId;
+    this.pageUrl = pageUrl;
+    this.referrer = referrer;
+    this.deviceType = deviceType;
+    this.browser = browser;
+    this.os = os;
+    this.country = country;
+    this.viewedAt = viewedAt;
+  }
+
+  static fromDTO(data: PageViewsDTO): PageViews {
+    return new PageViews(
+      data.id,
+      data.websiteId,
+      data.visitorId,
+      data.pageUrl,
+      data.referrer,
+      data.deviceType,
+      data.browser,
+      data.os,
+      data.country,
+      data.viewedAt
+    );
+  }
+
+  // Getters
+  getId(): number {
+    return this.id;
+  }
+  getWebsiteId(): number {
+    return this.websiteId;
+  }
+  getVisitorId(): string {
+    return this.visitorId;
+  }
+  getPageUrl(): string {
+    return this.pageUrl;
+  }
+  getReferrer(): string {
+    return this.referrer;
+  }
+  getDeviceType(): string {
+    return this.deviceType;
+  }
+  getBrowser(): string {
+    return this.browser;
+  }
+  getOS(): string {
+    return this.os;
+  }
+  getCountry(): string {
+    return this.country;
+  }
+  getViewedAt(): Date {
+    return this.viewedAt;
+  }
+}
