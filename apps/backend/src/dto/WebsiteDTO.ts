f4de96ee6f9d7ec6ba6fddb11ceb6ca06014bb2b
Tomas <tomas@debian.com>
2025-08-28 18:29:56 -0300

    add: create and update website DTOs

--- apps/backend/src/dto/WebsiteDTO.ts
+++ apps/backend/src/dto/WebsiteDTO.ts
@@ -7,3 +7,16 @@ export interface WebsiteDTO {
   createdAt: Date;
   updatedAt: Date;
 }
+
+export interface CreateWebsiteDTO {
+  userId: number;
+  siteName: string;
+  siteUrl: string;
+}
+
+export interface UpdateWebsiteDTO {
+  id: number;
+  userId: number;
+  siteName: string;
+  siteUrl: string;
+}
