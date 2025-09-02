2c0a767c5416db83413da0f183b9b35c2c987747
Tomas <tomas@debian.com>
2025-09-02 20:40:48 -0300

    add: Website Stats entity

---
+++ apps/backend/src/domain/entities/Stats.ts
@@ -0,0 +1,44 @@
+import { StatsRows } from "../../dto/PageViewDTO";
+
+export class Stats {
+  public uniqueVisitors: number;
+  public totalVisits: number;
+  public totalPageViews: number;
+  public viewsPerVisit: number;
+  public bounceRate: number;
+  public visitDuration: number;
+  public topPages: Array<StatsRows>;
+  public topSource: Array<StatsRows>;
+  public countries: Array<StatsRows>;
+  public browsers: Array<StatsRows>;
+  public oses: Array<StatsRows>;
+  public devices: Array<StatsRows>;
+
+  constructor(
+    uniqueVisitors: number,
+    totalVisits: number,
+    totalPageViews: number,
+    viewsPerVisit: number,
+    bounceRate: number,
+    visitDuration: number,
+    topPages: Array<StatsRows>,
+    topSource: Array<StatsRows>,
+    countries: Array<StatsRows>,
+    browsers: Array<StatsRows>,
+    oses: Array<StatsRows>,
+    devices: Array<StatsRows>
+  ) {
+    this.uniqueVisitors = uniqueVisitors;
+    this.totalVisits = totalVisits;
+    this.totalPageViews = totalPageViews;
+    this.viewsPerVisit = viewsPerVisit;
+    this.bounceRate = bounceRate;
+    this.visitDuration = visitDuration;
+    this.topPages = topPages;
+    this.topSource = topSource;
+    this.countries = countries;
+    this.browsers = browsers;
+    this.oses = oses;
+    this.devices = devices;
+  }
+}
